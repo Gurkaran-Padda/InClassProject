fde7d910262dc51a4ee6afe7eaa046d5a0da7d5f
Gurkaran-Padda <32564055+Gurkaran-Padda@users.noreply.github.com>
2019-10-01 10:31:50 -0400

    Added Splash Screen

--- app/src/main/java/com/ceng319/week3/MainActivity.java
+++ app/src/main/java/com/ceng319/week3/MainActivity.java
@@ -1,16 +1,20 @@
 package com.ceng319.week3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Toast Button
     protected Button myButton;
     String myString = "Hello this is my first android APP";
 
+    //Splash Screen time delay
+     private static int splash_delay = 300;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),myString,Toast.LENGTH_LONG).show();
             }
         });
+
+
     }
 }
